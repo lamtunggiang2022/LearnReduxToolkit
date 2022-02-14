30f3cf1ca47bc429b983354779d82e6a109851a5
lamtunggiang2022 <lamtunggiang.it@gmail.com>
2022-02-15 04:21:45 +0700

    to correct errors

--- ios/LearnReduxToolkit/AppDelegate.m
+++ ios/LearnReduxToolkit/AppDelegate.m
@@ -1,8 +1,13 @@
 #import "AppDelegate.h"
 
+#if RCT_DEV
+#import <React/RCTDevLoadingView.h>
+#endif
+
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -32,6 +37,12 @@ static void InitializeFlipper(UIApplication *application) {
 #endif
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  
+  
+#if RCT_DEV
+   [bridge moduleForClass:[RCTDevLoadingView class]];
+ #endif
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"LearnReduxToolkit"
                                             initialProperties:nil];
--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import store from './store'
 import Navigation from './navigation'
+import { LogBox } from 'react-native';
+
 
 export default function App() {
+  LogBox.ignoreLogs([
+    "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
+  ]);
   return (
-    <Provider store={store}>
+
+      <Provider store={store}>
         <Navigation />
-    </Provider>
+      </Provider>
+
+
   )
 }
